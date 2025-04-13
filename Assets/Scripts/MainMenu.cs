befcaca276dc9a1c0eac0d38f4ab052c38a0d596
jaakop <jaakko@kronogra.fi>
2025-04-13 09:00:11 +0300

    Main menu functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private bool canStart = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            if(Input.GetMouseButtonDown(0))
+                return;
+
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        if(!canStart)
+            return;
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetCanStart(bool canStart)
+    {
+        this.canStart = canStart;
+    }
+}
